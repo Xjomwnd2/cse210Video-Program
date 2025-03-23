24be83de83ddd4a852357abe1c1758ee941ca551
YourGitHubUsername <jmwaura@byupathway.edu>
2025-03-24 02:37:19 +0300

    COMMIT

--- YouTubeVideos/obj/Debug/net10.0/YouTubeVideos.AssemblyInfo.cs
+++ YouTubeVideos/obj/Debug/net10.0/YouTubeVideos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf172f1f03f3795211ef7b669a74225868845b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7829fe2961f60f16e1ad5367047ead4531c1de51")]
 [assembly: System.Reflection.AssemblyProductAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
